d90950e2ad13305f842ff51cf5cd9642659c7fe4
Tapish <tapishbalodi7@gmail.com>
2022-05-02 23:01:06 +0530

    installing ocket.io

--- server/index.js
+++ server/index.js
@@ -4,14 +4,15 @@ const io = require('socket.io')(8000);
 const user={};
 
 io.on('connection',socket=>{
-    socket.on('new-user-joined',name=>{
+    socket.on('new-user-joined', name=>{
+           console.log("name",name);
             user[socket.id]=name;
             socket.broadcast.emit('user-joined',name);
-
     })
+    
 
     socket.on('send',message=>{
         socket.broadcast.emit('receive',{message:message,name:user[socket.id]})
     });
-    
+
 })
