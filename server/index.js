eb3b0a4245aac45cd9b54cbb2f89ad37417c0382
Tapish <tapishbalodi7@gmail.com>
2022-05-02 15:59:47 +0530

    intalled ocket.io

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Node server to handle socket io connections
+const io = require('socket.io')(8000);
+
+const user={};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+            user[socket.id]=name;
+            socket.broadcast.emit('user-joined',name);
+
+    })
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message:message,name:user[socket.id]})
+    });
+    
+})
