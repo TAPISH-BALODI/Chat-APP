36ec6f3f2e4db4b4c32baaf24175dd9bd291bc9a
Tapish <tapishbalodi7@gmail.com>
2022-05-04 03:06:11 +0530

    responsive chat app complete

--- client/client.js
+++ client/client.js
@@ -1,9 +1,45 @@
 const socket=io('http://localhost:8000');
-
 const form=document.getElementById('send-container');
-const messageInput= document.getElementById("messageInp");
-const messageContainer= document.querySelector(".container");
+const messageInput=document.getElementById('messageInp');
+const messageContainer= document.querySelector('.container');
+var audio= new Audio('ding-sound-effect_2.mp3');
+var leftaudio= new Audio('mixkit-cowbell-sharp-hit-1743.wav');
+
+const append=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left')
+    {
+
+        audio.play();
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You : ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value='';
+})
+const name=prompt("enter your name to join");
 
 
-const name=prompt('Enter your name to join');
 socket.emit('new-user-joined',name);
+
+
+socket.on('user-joined',name=>{
+append(`${name} joined the chat`,'right');
+})
+
+socket.on('receive',data=>{
+    append(`${data.name}:${data.message} `,'left');
+})
+
+socket.on('left',name=>{
+    append(`${name} Left the Chat... `,'left');
+    leftaudio.play();
+})
